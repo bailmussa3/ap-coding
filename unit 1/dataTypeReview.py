f4d36854f8a4ce01edb3dc4cd40f55958e5190cc
bailmussa3 <mbaikro869@boyslatin.net>
2025-08-26 15:13:55 +0000

    first commit

---
+++ unit 1/dataTypeReview.py
@@ -0,0 +1,59 @@
+print('mussa Baikro')
+
+# W3Schools.com - our friend
+
+
+#string
+print("hello world")
+
+
+
+#boolean
+print(true)
+print(false)
+b = 3
+a = 2
+print(b > a)
+
+
+# interger
+print(123)
+
+#float
+print(3.14)
+
+#interger has no decimal float does.
+# 2.
+my_list = [
+    10, 20,          # Integers
+    3.14, 2.71,      # Floats
+    "hello", "world", # Strings
+    True, False,     # Booleans
+   
+]
+food = ["potato","salad","candy"]
+print([True, False, 10, 100, 10.5, 10.10, "I like apples"])
+print(my_list)
+
+
+school = "boys latin"  # string
+phoneNumber = 215321344 # interger
+isenrolled = True # boolean
+
+# assigntment
+# = single equal sign - assignment operator
+# == double equal sign - comparison operator specifically same as
+# !=
+# > greater than
+# < less than
+# >=
+# <=
+print(2 == 2)
+
+
+# logical operator - used to compare conditions.
+print(2 > 1 and 3 == 3) # true
+print(5 * 3 or 20 > 10) # true
+
+# arthematic operator
+
