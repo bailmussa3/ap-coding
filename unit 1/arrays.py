28a33e3d295e84d6190d24654c5304c34fb5e53e
bailmussa3 <mbaikro869@boyslatin.net>
2025-09-03 15:15:19 +0000

    done

--- unit 1/arrays.py
+++ unit 1/arrays.py
@@ -1,12 +1,12 @@
 numbersList[1,20,39,48,72,83]
 value = 48
 
-def numberThing(numberList,vaule) 
+def numberThing(numberList,vaule): 
   for x in numberList:
-  print (x)
- if x == value:
-    break
+    print (x)
+    if x == value:
+        break
  
 
 
-  NumberThing(numberList,value)
+numberThing (numbersList,value)
