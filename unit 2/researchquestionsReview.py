2e1d0b44354d3838e9a3373e8704484e206e90fe
bailmussa3 <mbaikro869@boyslatin.net>
2025-11-18 16:12:05 +0000

    assigment done 11/18/25

---
+++ unit 2/researchquestionsReview.py
@@ -0,0 +1,31 @@
+when answring the questons and recording our results, be sure to include 
+# the folowing 
+# identify what type of question it is; is it comparative descriptive,
+# or relational, ande explain why
+# is the intial question clear, focused . measureable, and insight,explain why.
+# which helper functions did you use and briefly explain why.
+# finally, answerthe question and describe why feel as through your
+# answer is coorect via your research and ir explain why you need to change
+# the intial question, if necessary
+
+# please write resposes as a string data + type
+2. Q: does defensive turnovers contribute to a team win percentage 
+“Type: Relational. 
+The question is clear and easy to measure.
+I used data from Harvard Sports Analysis. 
+Answer: Yes, more defensive turnovers help teams win.
++1 turnover is about 69.6% wins, +2 is 83.9%, +3 is 90.7%. 
+This is right because the data shows it.
+No change to the question.”
+
+
+
+
+
+3. Q. who has the most passing yard of all time?
+“Type: Descriptive.
+The question is clear and simple. 
+I used NFL record lists.
+Answer: Tom Brady has the most passing yards with 89,214.
+This is right because many sources say the same. 
+No change to the question.”
