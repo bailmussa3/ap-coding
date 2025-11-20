6f23db2f208e9888142a378e400c9e952e28ccb2
bailmussa3 <mbaikro869@boyslatin.net>
2025-11-20 15:53:09 +0000

    assigment completed 11/20

---
+++ unit 2/researchAnalysis.py
@@ -0,0 +1,16 @@
+
+
+describe what type of reaseach question the prmpt is and explain wh.
+explain which helper functions you used and how came to your mother
+
+# 11. which divison had the strongest defensive bade on yard allowed a game in 2024?
+The Eagles had the fewest yards allowed, I know  just that the Eagles’ defense gave up the least yards.
+
+
+# 12. does time of possesion correlate with winning percentage in the 2024 nfl season?
+No, not really. Some teams held the ball a lot and still didn’t win much, so it wasn’t a strong connection in 2024.
+
+
+# 13.which wr had the most targets vs thier receptions (catches) in 2024?
+
+ Ja’Marr Chase had the most targets with 175, and he caught 127 of them. So he had the biggest gap between targets and catches.
